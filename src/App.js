28bd59b6d639ee024d64a3b860ac17055cd19fc5
github username <itsDario1@gmail.com>
2019-05-11 19:18:07 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -10,21 +10,18 @@ import TestComponent from './TestComponent'
 class App extends Component {
   render() {
 
-    return ( <
-      div className = "App" >
-      <
-      header className = "App-header" >
-      <
-      p > Now < /p> <
-      /header> <
-      p className = "App-intro" >
-      In React apps, we write JSX - it looks like HTML, and uses a lot HTML syntax.JSX lets us include JavaScript functions right along with the HTML, and also allows us to add in components, which are separate, self - contained chunks of of JSX. <
-      /p> <
-      ExampleComponent / >
-      <
-      TestComponent / >
-      <
-      /div>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p> Now</p>
+        </header>
+        <p className="App-intro">
+          In React apps, we write JSX - it looks like HTML, and uses a lot HTML syntax.JSX lets us include JavaScript functions right along with the HTML, and also allows us to add in components, which are separate, self - contained chunks of of JSX.
+        </p>
+        <ExampleComponent />
+        <TestComponent />
+      </div>
+
     );
   }
 }
